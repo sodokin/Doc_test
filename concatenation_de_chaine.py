94c4ad97e368b96ff3f74b4c09a0d54362a67aa1
sodokin <ksodokin_s@yahoo.fr>
2020-11-19 20:31:48 +0100

    cette fois ça fonctionne

---
+++ concatenation_de_chaine.py
@@ -0,0 +1,8 @@
+#!/home/koudjo/python3.8.0
+# -*-coding:Utf-8 -*
+import os
+
+bob = "Hello"
+jul = "world!"
+salut = bob + "  " + jul
+print(salut)
---
+++ parcours_while.py
@@ -0,0 +1,10 @@
+#!/home/koudjo/python3.8.0
+# -*-coding:Utf-8 -*
+
+import os
+
+bob = "Emmanuelle"
+i = 0
+while i < len(bob):
+    print(bob[i])
+    i+=1
